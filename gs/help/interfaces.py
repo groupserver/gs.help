b34848bfdcf8fc79bb0e7b742fc3d1f86376b22e
Michael JasonSmith <mpj17@onlinegroups.net>
2012-10-23 12:20:09 +1100

    Adding a IGSHelp marker interface.

--- gs/help/interfaces.py
+++ gs/help/interfaces.py
@@ -1,13 +1,13 @@
 # coding=utf-8
 """Interfaces for the the help viewlets pages."""
-from zope.interface import Attribute
+from zope.interface.interface import Interface
 from zope.viewlet.interfaces import IViewletManager
-from zope.schema import List, Text, Bool, Choice
-from zope.schema.vocabulary import SimpleTerm, SimpleVocabulary
+
+class IGSHelp(Interface):
+    '''A marker interface for the Help system.'''
 
 class IUserHelp(IViewletManager):
     '''A viewlet manager for user help'''
 
 class IAdminHelp(IViewletManager):
     '''A viewlet manager for administration help'''
-
