35b45d2ce7e8a29c93b68b45e68c94ec1dd0659c
Michael JasonSmith <mpj17@onlinegroups.net>
2012-10-24 09:17:33 +1100

    Removing the old adminhelp page

--- gs/help/adminhelp.py
+++
@@ -1,9 +0,0 @@
-# coding=utf-8
-from Products.Five import BrowserView
-from zope.component import createObject
-
-class AdminHelp(BrowserView):
-    def __init__(self, site, request):
-        BrowserView.__init__(self, site, request)
-        self.siteInfo = createObject('groupserver.SiteInfo', site)
-
